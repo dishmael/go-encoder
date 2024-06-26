acae3c785d561b9829656cadaff863ad805bf68a
David Ishmael <dishmael3@gmail.com>
2024-06-26 13:33:27 -0400

    using args vs hard coded media file

--- main.go
+++ main.go
@@ -12,19 +12,16 @@ func main() {
 		return
 	}
 
-	/*
-		mi := GetInstance(os.Args[1])
-		br := mi.getAudioBitRate(0)
-		Logger.Infof("BitRate for Audio 0: %d\n", br)
-	*/
-
-	//fileName := "TV Title - S01E01 - First Episode (2023) Orig.mkv"
-	//fileName := "Movie Title (2023) Orig.mp4"
-
 	mf, err := NewMediaFile(os.Args[1])
 	if err != nil {
 		Logger.Errorf("%v\n", err)
 		os.Exit(-1)
 	}
 	Logger.Debugf("%+v\n", mf)
+
+	/*
+		mi := GetInstance(os.Args[1])
+		br := mi.getAudioBitRate(0)
+		Logger.Infof("BitRate for Audio 0: %d\n", br)
+	*/
 }
